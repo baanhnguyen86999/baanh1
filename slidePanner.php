de7175563cc2f97aed51366795577a91701390fa
Baanh <baanhnguyen86999@gmail.com>
2021-05-05 23:00:57 -0700

    first commit

---
+++ slidePanner.php
@@ -0,0 +1,14 @@
+<div class="slider slide-banner">
+  <div>
+    <div class="cont-item d-block w-100 text-center"><img class="w-100" src="./assets/img/1.jpg" alt=""></div>
+  </div>
+  <div>
+    <div class="cont-item d-block w-100 text-center"><img class="w-100" src="./assets/img/2.jpg" alt=""></div>
+  </div>
+  <div>
+    <div class="cont-item d-block w-100 text-center"><img class="w-100" src="./assets/img/3.jpg" alt=""></div>
+  </div>
+  <div>
+    <div class="cont-item d-block w-100 text-center"><img class="w-100" src="./assets/img/4.jpg" alt=""></div>
+  </div>
+</div>
